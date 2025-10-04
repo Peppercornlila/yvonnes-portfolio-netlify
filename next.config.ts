dd2b504bdf33c2a48d71eaeb9efe9d687abdaeee
Peppercornlila <y.a.mueller@gmail.com>
2025-10-04 16:14:49 +0200

    Fix TypeScript error in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+// next.config.ts
+import type { NextConfig } from 'next';
 
+// Erweitere die NextConfig-Schnittstelle, um turbopack zu erlauben
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {},
 };
 
 export default nextConfig;
